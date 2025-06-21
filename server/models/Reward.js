66bb757c38608330a9aec9688e4134a8e065576b
DevDanielC <correosdaniel01@gmail.com>
2025-06-21 10:45:30 -0500

    actualizaciom

--- server/models/Reward.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-   const rewardSchema = new mongoose.Schema({
-     name: { type: String, required: true },
-     description: { type: String },
-     pointsRequired: { type: Number, required: true },
-   });
-
-   module.exports = mongoose.model('Reward', rewardSchema);
